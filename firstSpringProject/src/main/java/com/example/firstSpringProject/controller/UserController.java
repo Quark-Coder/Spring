848ba83a80efd3b7266a8f993ac9a2368c4e0de2
Quark-Coder <zuchokmt10@gmail.com>
2022-10-02 22:10:40 +0300

    1.0

--- firstSpringProject/src/main/java/com/example/firstSpringProject/controller/UserController.java
+++ firstSpringProject/src/main/java/com/example/firstSpringProject/controller/UserController.java
@@ -1,13 +1,30 @@
 package com.example.firstSpringProject.controller;
 
+import com.example.firstSpringProject.entity.UserEntity;
+import com.example.firstSpringProject.exception.UserAlreadyExistException;
+import com.example.firstSpringProject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity registration(@RequestBody UserEntity user){
+        try {
+            userService.registration(user);
+            return ResponseEntity.ok("User saved succesfully.");
+        } catch(UserAlreadyExistException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch(Exception e){
+            return ResponseEntity.badRequest().body("Server is not working....");
+        }
+    }
 
     @GetMapping
     public ResponseEntity getUsers() {
